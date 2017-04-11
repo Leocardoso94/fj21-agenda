b1b649924a050c3f5f7d3af1855c02ae6afe88fb
Leonardo <leocardosoti@gmail.com>
2017-04-11 14:11:53 -0300

    Backup da base SQL

---
+++ dump.sql
@@ -0,0 +1,55 @@
+/*
+SQLyog Ultimate v12.09 (32 bit)
+MySQL - 5.5.16 : Database - fj21
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`fj21` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `fj21`;
+
+/*Table structure for table `contatos` */
+
+DROP TABLE IF EXISTS `contatos`;
+
+CREATE TABLE `contatos` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `endereco` varchar(255) DEFAULT NULL,
+  `dataNascimento` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Data for the table `contatos` */
+
+insert  into `contatos`(`id`,`nome`,`email`,`endereco`,`dataNascimento`) values (2,'leo','leocardosoti@gmail.com','Av. Rebouças','1994-10-10'),(3,'João','joao@joao.com','Av. Rebouças','1994-10-10');
+
+/*Table structure for table `funcionarios` */
+
+DROP TABLE IF EXISTS `funcionarios`;
+
+CREATE TABLE `funcionarios` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) DEFAULT NULL,
+  `usuario` varchar(255) DEFAULT NULL,
+  `senha` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `funcionarios` */
+
+insert  into `funcionarios`(`id`,`nome`,`usuario`,`senha`) values (1,'Joao','jaozika','1234');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
